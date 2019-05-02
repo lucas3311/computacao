56d5afb5f6c7c7d581fe3aa49749ca5e2447bab6
lucas3311 <lucaslimareis3311@gmail.com>
2019-05-02 15:44:33 -0300

    fixes

--- Computacao/Assets/Scripts/DialogueActor.cs
+++ Computacao/Assets/Scripts/DialogueActor.cs
@@ -19,7 +19,7 @@ public class DialogueActor : MonoBehaviour {
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null)
+            if (hit.collider == gameObject.GetComponent<Collider2D>())
             {
                 Debug.Log(hit.collider.gameObject.name);
                 //hit.collider.attachedRigidbody.AddForce(Vector2.up);
