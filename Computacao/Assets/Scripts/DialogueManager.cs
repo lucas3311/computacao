5880cde1fc978f839882537e48b0ee43ef168f7f
lucas3311 <lucaslimareis3311@gmail.com>
2019-05-02 15:02:30 -0300

    Dialogue system 0.1

---
+++ Computacao/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Text text;
+    bool onDialogue = false;
+    public int lineNumber = 0;
+    // Use this for initialization
+    void Start () {
+
+        text.enabled = false;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    public void lineDialogue(string[] Dialogue)
+    {
+        onDialogue = true;
+        text.enabled = true;
+        
+        
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            lineNumber += 1;
+            if (lineNumber == Dialogue.Length)
+            {
+                text.enabled = false;
+                
+                onDialogue = false;
+
+                lineNumber = 0;
+            }
+        }
+
+        
+    }
+}
