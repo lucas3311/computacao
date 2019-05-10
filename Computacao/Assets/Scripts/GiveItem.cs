dd3fd6f13f06d7fd814c4d762262a5270b702b41
lucas3311 <lucaslimareis3311@gmail.com>
2019-05-10 19:02:42 -0300

    a

---
+++ Computacao/Assets/Scripts/GiveItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GiveItem : MonoBehaviour
+{
+    public GameObject[] items;
+    public Sprite[] ItemSprites;
+    public int itemToGive;
+    // Use this for initialization
+    public void giveItem(int itemNumber)
+    {
+        items[itemNumber].SetActive(true);
+        items[itemNumber].transform.GetChild(0).gameObject.GetComponent<Image>().sprite = ItemSprites[itemNumber];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider == gameObject.GetComponent<Collider2D>())
+            {
+
+                giveItem(itemToGive);
+            }
+        }
+    }
+}
